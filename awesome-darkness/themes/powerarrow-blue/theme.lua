7ad7808c0098c1ef58c26c9d8ead70b092a8ec63
megasuperfish <jvseters01+github@gmail.com>
2024-01-08 16:52:27 +0100

    main os

---
+++ awesome-darkness/themes/powerarrow-blue/theme.lua
@@ -0,0 +1,312 @@
+--[[
+
+     Powerarrow Awesome WM theme
+     github.com/lcpz
+
+--]]
+
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+
+local math, string, os = math, string, os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local theme                                     = {}
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/powerarrow-blue"
+theme.font                                      = "DejaVu Sans Mono Bold 10"
+theme.taglist_font                              = "Font Awesome 6 Brands Regular 16"
+theme.fg_normal                                 = "#ffffff"
+theme.fg_blue                                   = "#174DCB"
+theme.fg_magenta                                = "#A0A0A0"
+theme.fg_focus                                  = "#6200C4"
+theme.fg_urgent                                 = "#b74822"
+theme.bg_normal                                 = "#111111"
+theme.bg_focus                                  = "#111111"
+theme.bg_urgent                                 = "#3F3F3F"
+theme.taglist_fg_focus                          = "#6200C4"
+theme.tasklist_bg_focus                         = "#000000"
+theme.tasklist_fg_focus                         = "#CC6600"
+theme.border_width                              = 2
+theme.border_normal                             = "#310053"
+theme.border_focus                              = "#6200C4"
+theme.border_marked                             = "#6200C4"
+theme.titlebar_bg_focus                         = "#3F3F3F"
+theme.titlebar_bg_normal                        = "#3F3F3F"
+theme.titlebar_bg_focus                         = theme.bg_focus
+theme.titlebar_bg_normal                        = theme.bg_normal
+theme.titlebar_fg_focus                         = theme.fg_focus
+theme.menu_height                               = 20
+theme.menu_width                                = 140
+theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
+theme.awesome_icon                              = theme.dir .. "/icons/awesome.png"
+theme.layout_tile                               = theme.dir .. "/icons/tile.png"
+theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.dir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.widget_ac                                 = theme.dir .. "/icons/ac.png"
+theme.widget_mem                                = theme.dir .. "/icons/mem.png"
+theme.widget_cpu                                = theme.dir .. "/icons/cpu.png"
+theme.widget_temp                               = theme.dir .. "/icons/temp.png"
+theme.widget_net                                = theme.dir .. "/icons/net.png"
+theme.widget_hdd                                = theme.dir .. "/icons/hdd.png"
+theme.widget_music                              = theme.dir .. "/icons/note.png"
+theme.widget_music_on                           = theme.dir .. "/icons/note.png"
+theme.widget_music_pause                        = theme.dir .. "/icons/pause.png"
+theme.widget_music_stop                         = theme.dir .. "/icons/stop.png"
+theme.widget_vol                                = theme.dir .. "/icons/vol.png"
+theme.widget_vol_low                            = theme.dir .. "/icons/vol_low.png"
+theme.widget_vol_no                             = theme.dir .. "/icons/vol_no.png"
+theme.widget_vol_mute                           = theme.dir .. "/icons/vol_mute.png"
+theme.widget_mail                               = theme.dir .. "/icons/mail.png"
+theme.widget_mail_on                            = theme.dir .. "/icons/mail_on.png"
+theme.widget_task                               = theme.dir .. "/icons/task.png"
+theme.widget_scissors                           = theme.dir .. "/icons/scissors.png"
+theme.widget_weather                            = theme.dir .. "/icons/dish.png"
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.useless_gap                               = 6
+theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+theme.bg_systray                                = "#111111"
+theme.clock_fontfg                              = "#8c8c8b"
+theme.clock_font                                = "DejaVu Sans Mono Bold 9"
+
+local markup = lain.util.markup
+local separators = lain.util.separators
+
+
+-- Textclock
+local clockicon = wibox.widget.imagebox(theme.widget_clock)
+local clock = awful.widget.watch(
+    "date +'%a %d %b %R'", 60,
+    function(widget, stdout)
+        widget:set_markup(" " .. markup.fontfg(theme.clock_font, theme.clock_fontfg, stdout))
+    end
+)
+
+-- Calendar
+theme.cal = lain.widget.cal({
+    attach_to = { clock },
+    notification_preset = {
+        font = "Roboto Mono Medium 10",
+        fg   = theme.border_focus,
+        bg   = theme.bg_normal
+    }
+})
+
+-- ALSA volume
+theme.volume = lain.widget.alsabar({
+    --togglechannel = "IEC958,3",
+    notification_preset = { font = theme.font, fg = theme.fg_blue },
+})
+
+-- MPD
+local musicplr = "urxvt -title Music -g 130x34-320+16 -e ncmpcpp"
+local mpdicon = wibox.widget.imagebox(theme.widget_music)
+mpdicon:buttons(my_table.join(
+    awful.button({ modkey }, 1, function () awful.spawn.with_shell(musicplr) end),
+    awful.button({ }, 2, function ()
+        awful.spawn.with_shell("mpc toggle")
+        theme.mpd.update()
+    end),
+    awful.button({ modkey }, 3, function () awful.spawn.with_shell("pkill ncmpcpp") end),
+    awful.button({ }, 3, function ()
+        awful.spawn.with_shell("mpc stop")
+        theme.mpd.update()
+    end)))
+theme.mpd = lain.widget.mpd({
+    settings = function()
+        if mpd_now.state == "play" then
+            artist = " " .. mpd_now.artist .. " "
+            title  = mpd_now.title  .. " "
+            mpdicon:set_image(theme.widget_music_on)
+            widget:set_markup(markup.font(theme.font, markup("#FFFFFF", artist) .. " " .. title))
+        elseif mpd_now.state == "pause" then
+            widget:set_markup(markup.font(theme.font, " mpd paused "))
+            mpdicon:set_image(theme.widget_music_pause)
+        else
+            widget:set_text("")
+            mpdicon:set_image(theme.widget_music)
+        end
+    end
+})
+
+-- MEM
+local memicon = wibox.widget.imagebox(theme.widget_mem)
+local mem = lain.widget.mem({
+    settings = function()
+        widget:set_markup(markup.font(theme.font, " " .. mem_now.used .. "MB "))
+    end
+})
+
+-- CPU
+local cpuicon = wibox.widget.imagebox(theme.widget_cpu)
+local cpu = lain.widget.cpu({
+    settings = function()
+        widget:set_markup(markup.font(theme.font, " " .. cpu_now.usage .. "% "))
+    end
+})
+
+-- Coretemp (lain, average)
+local temp = lain.widget.temp({
+    settings = function()
+        widget:set_markup(markup.font(theme.font, " " .. coretemp_now .. "°C "))
+    end
+})
+--]]
+local tempicon = wibox.widget.imagebox(theme.widget_temp)
+
+-- ALSA volume
+local volicon = wibox.widget.imagebox(theme.widget_vol)
+theme.volume = lain.widget.alsa({
+    settings = function()
+        if volume_now.status == "off" then
+            volicon:set_image(theme.widget_vol_mute)
+        elseif tonumber(volume_now.level) == 0 then
+            volicon:set_image(theme.widget_vol_no)
+        elseif tonumber(volume_now.level) <= 50 then
+            volicon:set_image(theme.widget_vol_low)
+        else
+            volicon:set_image(theme.widget_vol)
+        end
+
+        widget:set_markup(markup.font(theme.font, " " .. volume_now.level .. "% "))
+    end
+})
+
+-- Separators
+local arrow = separators.arrow_left
+
+function theme.powerline_rl(cr, width, height)
+    local arrow_depth, offset = height/2, 0
+
+    -- Avoid going out of the (potential) clip area
+    if arrow_depth < 0 then
+        width  =  width + 2*arrow_depth
+        offset = -arrow_depth
+    end
+
+    cr:move_to(offset + arrow_depth         , 0        )
+    cr:line_to(offset + width               , 0        )
+    cr:line_to(offset + width - arrow_depth , height/2 )
+    cr:line_to(offset + width               , height   )
+    cr:line_to(offset + arrow_depth         , height   )
+    cr:line_to(offset                       , height/2 )
+
+    cr:close_path()
+end
+
+local function pl(widget, bgcolor, padding)
+    return wibox.container.background(wibox.container.margin(widget, 16, 16), bgcolor, theme.powerline_rl)
+end
+
+function theme.at_screen_connect(s)
+    -- Quake application
+   -- s.quake = lain.util.quake({ app = awful.util.terminal })
+   s.quake = lain.util.quake({ app = "termite", height = 0.50, argname = "--name %s" })
+
+    -- If wallpaper is a function, call it with the screen
+    local wallpaper = theme.wallpaper
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+
+    -- All tags open with layout 1
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(my_table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create the wibox
+    function custom_shape(cr, width, height)
+
+        gears.shape.rounded_rect(cr, width, height, 25)
+
+    end
+
+    s.mywibox = awful.wibar({ position = "top", screen = s, shape = custom_shape, height = 35, width = 1850, border_width = 5, bg = theme.bg_normal, fg = theme.fg_magenta })
+
+    tbox_separator = wibox.widget.textbox(" | ")
+
+    local vert_sep = wibox.widget {
+        widget = wibox.widget.separator,
+        orientation = "horizontal",
+        forced_width = 10,
+        color = "#6200C4",
+    }
+    
+    local fedora_logo = wibox.widget {
+		widget = wibox.widget.textbox,
+		text = "linux",
+		font = "Font Awesome 6 Brands Regular 18"
+    }
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            --spr,
+            wibox.container.background(wibox.container.margin(fedora_logo, 20, 15, 2, 5)),
+            vert_sep,
+            wibox.container.background(wibox.container.margin(clock, 6, 0, 2, 2)),
+            s.mypromptbox,
+            spr,
+        },
+        {
+            layout = wibox.layout.align.horizontal,
+            wibox.container.background(wibox.container.margin(s.mytaglist, 530, 0, 2, 2)),
+        },
+        -- s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            wibox.container.background(wibox.container.margin(wibox.widget { theme.volume.widget, layout = wibox.layout.align.horizontal }, 2, 3)),
+            vert_sep,
+            wibox.container.background(wibox.container.margin(wibox.widget { mem.widget, layout = wibox.layout.align.horizontal }, 2, 3)),
+            vert_sep,
+            wibox.container.background(wibox.container.margin(wibox.widget { temp.widget, layout = wibox.layout.align.horizontal }, 4, 4)),
+            vert_sep,
+            wibox.container.background(wibox.container.margin(wibox.widget.systray { layout = wibox.layout.align.horizontal }, 8, 4)),
+           -- s.mylayoutbox,
+        },
+    }
+end
+
+return theme
---
+++ install.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+intro() {
+    echo
+    echo -e "Use the corresponding number to make a selection"
+    echo
+    echo -e "\e[91m{1}      \e[32mDownload + Install Programs"
+    echo -e "\e[91m{2}      \e[32mInstall Awesomewm - Darkness"
+    echo -e "\e[91m{99}     \e[32mExit\e[0m"
+    echo
+    read -p "Selection: " menuResponse
+
+    if [ "$menuResponse" == "99" ]
+    then
+        exit 1
+    fi
+
+    if [ "$menuResponse" == "1" ]
+    then
+        installPrograms
+    fi
+
+    if [ "$menuResponse" == "2" ]
+    then
+        awesomeDarkness
+    fi
+}
+
+awesomeDarkness() {
+
+    clear;
+
+    echo -e "You have selected to install Awesomewm - Darkness!"
+    echo -e "This includes the following config installs:"
+    echo -e "1. Awesomewm Darkness Desktop Theme"
+    echo -e "2. Kitty Terminal Emulator"
+    echo -e "3. Rofi"
+    echo -e "4. Vim"
+    echo -e "5. ZSH"
+    echo -e "6. Picom"
+    echo -e "7. Geany"
+    echo -e "8. Startpage"
+    echo -e "If existing configs are already in place, they will be renamed with a .old prefix"
+    echo -e "Do you wish to continue? (y/n)"
+    echo
+    read -p "Selection: " darknessResponse
+
+    if [ "$darknessResponse" == "n" ]
+    then
+        intro
+    fi
+
+    if [ "$darknessResponse" == "y" ]
+    then
+
+        echo -e "Checking for existing configs..."
+
+        if [ -d "/home/$USER/.config/awesome" ]
+        then
+            mv /home/$USER/.config/awesome ~/.config/awesome.old
+            echo -e "Awesomewm config found, renamed to awesome.old"
+        fi
+
+        if [ -d "/home/$USER/.config/kitty" ]
+        then
+            mv /home/$USER/.config/kitty ~/.config/kitty.old
+            echo -e "Kitty config found, renamed to kitty.old"
+        fi
+
+        if [ -d "/home/$USER/.config/rofi" ]
+        then
+            mv /home/$USER/.config/rofi ~/.config/rofi.old
+            echo -e "Rofi config found, renamed to rofi.old"
+        fi
+
+        if [ -d "/home/$USER/.config/picom" ]
+        then
+            mv /home/$USER/.config/picom ~/.config/picom.old
+            echo -e "Picom config found, renamed to picom.old"
+        fi
+
+        if [ -d "/home/$USER/.config/geany" ]
+        then
+            mv /home/$USER/.config/geany ~/.config/geany.old
+            echo -e "Geany config found, renamed to geany.old"
+        fi
+
+        if [ -d "/home/$USER/Startpage" ]
+        then
+            mv /home/$USER/Startpage /home/$USER/Startpage.old
+            echo -e "Startpage found, renamed to Startpage.old"
+        fi
+
+        echo
+        echo
+        echo -e "Installing new config files..."
+        echo
+        cp -r awesome-darkness/ ~/.config/awesome
+        echo -e "Awesome - Darkness installed!"
+        cp -r kitty/ ~/.config/
+        echo -e "Kitty config installed!"
+        cp -r rofi/ ~/.config/
+        echo -e "Rofi config installed!"
+        cp vim/.vimrc /home/$USER/.vimrc
+        echo -e "Vimrc installed!"
+        cp zsh/.zshrc /home/$USER/.zshrc
+        cp zsh/chrollo.zsh-theme /home/$USER/.oh-my-zsh/themes/
+        echo -e "ZSH config installed!"
+        cp -r picom/ ~/.config/picom
+        echo -e "Picom config installed!"
+        cp -r geany/ ~/.config/geany
+        echo -e "Geany config installed!"
+        cp -r Startpage/ /home/$USER/
+        echo -e "Startpage installed!"
+    fi
+}
+
+installPrograms() {
+
+    clear;
+
+    echo -e "Select which distro you are using:"
+    echo -e "\e[91m{1}      \e[32mFedora    (dnf)"
+    echo -e "\e[91m{2}      \e[32mDebian    (apt)"
+    echo -e "\e[91m{3}      \e[32mArch      (pacman)"
+    echo -e "\e[91m{99}     \e[32mExit\e[0m"
+    echo
+    read -p "Selection: " distroResponse
+
+    if [ "$distroResponse" == "99" ]
+    then
+        intro
+    fi
+
+    if [ "$distroResponse" == "1" ]
+    then
+        sudo dnf install awesome geany kitty picom rofi zsh
+    fi
+
+    if [ "$distroResponse" == "2" ]
+    then
+        sudo apt install awesome geany kitty picom rofi zsh 
+    fi
+
+    if [ "$distroResponse" == "3" ]
+    then
+        sudo pacman -S awesome geany kitty picom rofi zsh
+
+    fi
+
+    echo -e "do you want more then the base packages"
+    
+}
+
+
+intro
